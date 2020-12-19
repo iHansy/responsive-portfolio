24617b07f8bd1e796ac882d0c695589dc400bd63
Hans Accola <Hansolo6283@gmail.com>
2020-12-18 19:58:51 -0600

    added img and description

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,15 +6,12 @@ function AboutMe(props) {
     const [state, setState] = useState('Starter state');
 
     return (
-        <div>
-            <div>
-                <img src="headshot-4.jpg" alt="Girl in a jacket" width="300" height="300"></img>
+        <div className="aboutme-container">
+            <div className="aboutme-img-container">
+                <img className="aboutme-img" src="headshot-4.jpg" alt="Headshot of Hans Accola" width="350" height="350"></img>
             </div>
-            <div>
-                <p>About me description goes here!</p>
-            </div>
-            <div>
-                
+            <div className="aboutme-description">
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
             </div>
         </div>
     );
