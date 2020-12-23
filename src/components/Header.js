f403d10ff0b13f32765a2bfffab595b3b5123817
Hans Accola <Hansolo6283@gmail.com>
2020-12-23 15:37:53 -0600

    added material ui styling to nav buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react';
-import { List, Button } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+//custom material ui styling
+const useStyles = makeStyles((theme) => ({
+    navBtn: {
+        '&:hover': {
+          backgroundColor: 'rgb(190, 200, 210)',
+          borderColor: '#0062cc',
+          boxShadow: 'none',
+        },
+    }
+}));
 
 function Header(props) {
 
-    // Using hooks we're creating local state
+    //for styles
+    const classes = useStyles();
+
+    // Example hook to create local state
     const [state, setState] = useState('Starter state');
 
     return (
@@ -13,10 +28,10 @@ function Header(props) {
             </div>
             <nav className="header-nav-container">
                 <ul className="header-nav-links">
-                    <li><Button className="btn-nav-link"><p className="header-nav-texts">About Me</p></Button></li>
-                    <li><Button className="btn-nav-link"><p className="header-nav-texts">Projects</p></Button></li>
-                    <li><Button className="btn-nav-link"><p className="header-nav-texts">Technologies</p></Button></li>
-                    <li><Button className="btn-nav-link"><p className="header-nav-texts">Contact</p></Button></li>
+                    <li><Button className={classes.navBtn}><p className="header-nav-texts">About Me</p></Button></li>
+                    <li><Button className={classes.navBtn}><p className="header-nav-texts">Projects</p></Button></li>
+                    <li><Button className={classes.navBtn}><p className="header-nav-texts">Technologies</p></Button></li>
+                    <li><Button className={classes.navBtn}><p className="header-nav-texts">Contact</p></Button></li>
                 </ul>
             </nav>
         </header>
