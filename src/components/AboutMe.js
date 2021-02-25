ac7d5e15caab22638f875e86ac2e9b23b7d5030c
Hans Accola <Hansolo6283@gmail.com>
2021-02-24 20:51:28 -0600

    just need to add pdf file to resume link

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,15 +12,15 @@ function AboutMe(props) {
             </div>
             <div className="aboutme-description">
                 <h1 className="aboutme-welcome"><span className="first-letter-color">W</span>ELCOME!</h1>
-                <p className="aboutme-p1">Hi there, I’m Hans Accola. Nice to meet you! I’m originally from Norway but moved to Minnesota when I was a child and have been here ever since. Here are a few of my favorite things to do outside of coding:</p>
+                <p className="aboutme-p1">Hi there, I’m Hans Accola. Nice to meet you! I’m originally from Norway but moved to Minnesota when I was a child and have lived here ever since. Here are a few of my favorite things to do outside of coding:</p>
                 <ul className="aboutme-list">
                     <li>Going on hikes with my amazing fiancé, Marie</li>
                     <li>Playing basketball and working out at the gym</li>
                     <li>Bringing my 95lb black lab to the doggo park</li>
                     <li>Taking typing speed tests on 10fastfingers.com, max so far is 150+ WPM</li>
                 </ul>
-                <p className="aboutme-p2">My professional experience started in 2017 as an inside medical sales rep. After that I worked in IT / IoT services testing indoor tracking devices (RTLS). Unfortunately, due to COVID I got laid off. But that opened a new door to pursue my dream of becoming a software engineer! With that spark I earned a certification in Full-Stack Software Engineering at Prime Digital Academy here in Minneapolis. Currently, I am learning Java through a course on Udemy.</p>
-                <h4 className="resume-link"><a href="">Click here to check out my resume.</a></h4>
+                <p className="aboutme-p2">My professional experience started in 2017 as an inside medical sales rep. After that I worked in IT / IoT services testing and configuring indoor tracking devices (RTLS). Unfortunately, due to COVID I got laid off. But that opened a new door to pursue my dream of becoming a software engineer! With that spark I earned a certification in Full-Stack Software Engineering at Prime Digital Academy here in Minneapolis. Currently, I am learning Java through a course on Udemy.</p>
+                <a className="resume-link" href="">Click here to check out my resume!</a>
             </div>
         </div>
     );
