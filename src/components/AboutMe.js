15eed45d1e6bc8f11aec57b822ed8493445d5368
Hans Accola <Hansolo6283@gmail.com>
2021-02-24 19:36:49 -0600

    added resume link, need to style and add resume

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -17,9 +17,10 @@ function AboutMe(props) {
                     <li>Going on hikes with my amazing fiancé, Marie</li>
                     <li>Playing basketball and working out at the gym</li>
                     <li>Bringing my 95lb black lab to the doggo park</li>
-                    <li>Taking typing speed tests on 10fastfingers.com, max WPM so far is 150+</li>
+                    <li>Taking typing speed tests on 10fastfingers.com, max so far is 150+ WPM</li>
                 </ul>
-                <p className="aboutme-p2">My professional experience started in 2017 as an inside medical sales rep. After that I worked in IT / IoT services testing indoor tracking devices (RTLS). Unfortunately, due to COVID I got laid off. But that opened a new door to pursue my dream of becoming a software engineer! With that spark I earned a certification in Full-Stack Software Engineering at Prime Digital Academy here in Minneapolis.</p>
+                <p className="aboutme-p2">My professional experience started in 2017 as an inside medical sales rep. After that I worked in IT / IoT services testing indoor tracking devices (RTLS). Unfortunately, due to COVID I got laid off. But that opened a new door to pursue my dream of becoming a software engineer! With that spark I earned a certification in Full-Stack Software Engineering at Prime Digital Academy here in Minneapolis. Currently, I am learning Java through a course on Udemy.</p>
+                <h4 className="resume-link"><a href="">Click here to check out my resume.</a></h4>
             </div>
         </div>
     );
