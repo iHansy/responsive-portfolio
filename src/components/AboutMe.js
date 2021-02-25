84555ca5579e2a6409e1c410cd653fcf4723a40f
Hans Accola <Hansolo6283@gmail.com>
2021-02-24 21:03:36 -0600

    linked resume as .pdf to resume link

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ function AboutMe(props) {
     return (
         <div className="aboutme-container">
             <div className="aboutme-img-container">
-                <img className="aboutme-img" src="headshot-4.jpg" alt="Headshot of Hans Accola"></img>
+                <img className="aboutme-img" src="/images/headshot-4.jpg" alt="Headshot of Hans Accola"></img>
             </div>
             <div className="aboutme-description">
                 <h1 className="aboutme-welcome"><span className="first-letter-color">W</span>ELCOME!</h1>
@@ -20,7 +20,7 @@ function AboutMe(props) {
                     <li>Taking typing speed tests on 10fastfingers.com, max so far is 150+ WPM</li>
                 </ul>
                 <p className="aboutme-p2">My professional experience started in 2017 as an inside medical sales rep. After that I worked in IT / IoT services testing and configuring indoor tracking devices (RTLS). Unfortunately, due to COVID I got laid off. But that opened a new door to pursue my dream of becoming a software engineer! With that spark I earned a certification in Full-Stack Software Engineering at Prime Digital Academy here in Minneapolis. Currently, I am learning Java through a course on Udemy.</p>
-                <a className="resume-link" href="">Click here to check out my resume!</a>
+                <a className="resume-link" href="/images/hans-accola-resume.pdf">Click here to check out my resume!</a>
             </div>
         </div>
     );
