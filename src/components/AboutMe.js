ce5cc4bcca5f1d15d001e590364b93a5dca63e40
Hans Accola <Hansolo6283@gmail.com>
2021-02-24 18:43:01 -0600

    updated text to about me page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,7 +11,15 @@ function AboutMe(props) {
                 <img className="aboutme-img" src="headshot-4.jpg" alt="Headshot of Hans Accola"></img>
             </div>
             <div className="aboutme-description">
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima?Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima?</p>
+                <h1>WELCOME!</h1>
+                <p>Hi there, I’m Hans Accola. Nice to meet you! I’m originally from Norway but moved to Minnesota when I was a child and have been here ever since. Here are a few of my favorite things to do outside of coding:</p>
+                <ul>
+                    <li>Going on hikes with my amazing fiancé, Marie</li>
+                    <li>Playing basketball and going to the gym</li>
+                    <li>Bringing my 95lb black lab to the doggo park</li>
+                    <li>Taking typing speed tests on 10fastfingers.com, max WPM so far is 150+</li>
+                </ul>
+                <p>My professional experience started in 2017 as an inside medical sales rep. After that I worked in IT / IoT services testing indoor tracking devices (RTLS). Unfortunately, due to COVID I got laid off. But that opened a new door to pursue my dream of becoming a software engineer! With that spark I earned a certification in Full-Stack Software Engineering at Prime Digital Academy here in Minneapolis.</p>
             </div>
         </div>
     );
