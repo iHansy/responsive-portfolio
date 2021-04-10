88487c70affeef15638235b71f225a63b6c87076
Hans Accola <Hansolo6283@gmail.com>
2021-04-10 02:25:39 -0500

    working on dynamic img for all technology icons

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
 import './Technologies.css';
+import IconAmazonwebservices from 'react-devicon/amazonwebservices/original'
 
 function Technologies(props) {
 
     // Using hooks we're creating local state
-    const [state, setState] = useState('Starter state');
+    const [state, setState] = useState({
+        icons: [
+            'css', 
+            'html', 
+            'java', 
+            'python', 
+            'javascript', 
+            'react', 
+            'vue', 
+            'mui', 
+            'node', 
+            'npm', 
+            'postgresql', 
+            'heroku', 
+            'github'
+        ]
+    });
 
     return (
-       <div className="technologies-container" id="Technologies">
-           <h1>Technologies go here</h1>
-       </div>
+        <div className="technologies-container" id="Technologies">
+            <h1>Technologies go here</h1>
+            {state.icons.map((icon, i) => {
+                <p>{icon}</p>
+            })}
+        </div>
     );
 };
 
