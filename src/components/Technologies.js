ce33e97e5b68774d96d7d950b2a798530b234be2
Hans Accola <Hansolo6283@gmail.com>
2021-04-16 23:36:22 -0500

    added splice to technology items to get technology name

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -13,35 +13,20 @@ export default function Technologies(props) {
     const classes = useStyles();
     const [spacing, setSpacing] = useState(6);
     const [state, setState] = useState({
-        iconImages: [
-            '/images/css.png',
-            '/images/html.png',
-            '/images/java.png',
-            '/images/python.png',
-            '/images/javascript.png',
-            '/images/react.png',
-            '/images/vue.png',
-            '/images/mui.png',
-            '/images/node.png',
-            '/images/npm.png',
-            '/images/postgresql.png',
-            '/images/heroku.png',
-            '/images/github.png'
-        ],
-        iconNames: [
-            'CSS3',
-            'HTML',
-            'Java',
-            'Python',
-            'Javascript',
-            'React',
-            'Vue',
-            'Material UI',
-            'Node.js',
-            'npm',
-            'postgreSQL',
-            'Heroku',
-            'Github',
+        technologyUrl: [
+            '/images/CSS.png',
+            '/images/HTML.png',
+            '/images/Java.png',
+            '/images/Python.png',
+            '/images/JavaScript.png',
+            '/images/React.js.png',
+            '/images/Vue.js.png',
+            '/images/MUI.png',
+            '/images/Node.js.png',
+            '/images/NPM.png',
+            '/images/postgreSQL.png',
+            '/images/Heroku.png',
+            '/images/Github.png'
         ]
     });
 
@@ -51,19 +36,19 @@ export default function Technologies(props) {
             <Grid container className={classes.root} alignItems="center">
                 <Grid item xs={12}>
                     <Grid container justify="center" spacing={spacing}>
-                        {state.iconImages.map((icon, i) => (
-                            <Grid item key={icon}>
-                                <img src={icon} alt={icon} />
-                                {state.iconNames.map((name, i) => {
-                                    return (
-                                        <Typography key={i}>
-                                            {name}testing
-                                        </Typography>
+                        {state.technologyUrl.map((url, i) => {
+                            const technologyName = url.split('/')[2].split('.png')[0]
+                            return (
+                                <Grid item key={i}>
+                                    <img src={url} alt={technologyName} />
+                                    <Typography>
+                                        {technologyName}
+                                    </Typography>
 
-                                    )
-                                })}
-                            </Grid>
-                        ))}
+                                </Grid>
+
+                            )
+                        })}
                     </Grid>
                 </Grid>
             </Grid>
